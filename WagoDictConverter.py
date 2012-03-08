c0195d18ec8808384db52f53604184ef0edbe8dd
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-09 04:47:29 +0900

    add doctest Test Failed

--- WagoDictConverter.py
+++ WagoDictConverter.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+import yaml
+from wagodict2codedict import wagodict2codedict
 
 class WagoDictConverter:
-    pass
+    def __init__(self, wagodict):
+        self.wagodict = wagodict
+
+    def do(self, codedict):
+        '''
+        >>> from walk import walk
+        >>> import yaml
+        >>> tests = yaml.load(open('test/WagoDictConverter.yaml').read())
+        >>> for t in tests:
+        ...  wc = WagoDictConverter(t['wagodict'])
+        ...  walk(t['codedictlist'], wc.do)
+        ...  assert t['wagodict'] == t['expected']
+
+        # ...  print (t['wagodict'])
+        # ...  print (t['expected'])
+        '''
+        wagodict2codedict(self.wagodict, codedict)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
