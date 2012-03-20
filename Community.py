a73e12e3593790f6cac43b4f7c6af8a741ee3894
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-21 03:22:08 +0900

    wagodict を codedict に変換するクラス追加。テスト通過

---
+++ Community.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import yaml
+from wagopath2filepath import wagopath2filepath
+from wagodict2codedict import wagodict2codedict
+
+class Community:
+    def bring_up(self, wagodict):
+        '''
+        >>> com = Community()
+        >>> tests = yaml.load(open('test/Community.bring_up.yaml').read())
+        >>> for t in tests:
+        ...  com.bring_up(t['wagodict'])
+        ...  assert t['wagodict'] == t['expected']
+
+        #...  print (t['wagodict'])
+        #...  print (t['expected'])
+        '''
+        filepath = wagopath2filepath(wagodict['wago'])
+        code = yaml.load(open(filepath).read())
+        codedict = code[0]
+        wagodict2codedict(wagodict, codedict)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
