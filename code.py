7af82dc2e03ca032baf56c0846c62cfff2765069
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-21 03:26:08 +0900

    add file

---
+++ code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import yaml
+from Community import Community
+from walk import walk
+
+def code(wagofile): pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
