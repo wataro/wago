af17648013cc5b3450bc46077e564ab84fb6dae4
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-21 03:51:46 +0900

    実装したがテスト通らない？

--- code.py
+++ code.py
@@ -2,8 +2,22 @@
 import yaml
 from Community import Community
 from walk import walk
+from wagopath2filepath import wagopath2filepath
 
-def code(wagofile): pass
+def code(wagofile):
+    '''
+    >>> tests = yaml.load(open('test/code.yaml').read())
+    >>> for t in tests:
+    ...  actual = code(t['wagofile'])
+    ...  print (actual)
+    ...  print (t['expected'])
+    #...  assert actual == t['expected']
+    '''
+    com = Community()
+    filepath = wagopath2filepath(wagofile)
+    baby = yaml.load(open(filepath).read())
+    walk(baby, com.bring_up)
+    return baby
 
 if __name__ == '__main__':
     import doctest
