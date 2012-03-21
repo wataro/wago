b434ad408e0e9c4d59b68321114a08245f282a8c
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-21 18:07:50 +0900

    remove files

--- walk.py
+++ walk.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 # 入れ子になっているリストを再帰的にたどりながら何かする
-def walkBody(data, func, depth=0):
+def walkBody(data, func):
     for i in data:
         if isinstance(i, list):
-            walkBody(i, func, depth+1)
+            walkBody(i, func)
         else:
-            func(i, depth)
+            func(i)
 
 def walk(data, func):
     walkBody(data, func)
