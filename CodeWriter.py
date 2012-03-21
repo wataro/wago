efb3c578ab3f752210c0b0ef2ac5fe7f7534034f
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-22 06:51:02 +0900

    実装した

--- CodeWriter.py
+++ CodeWriter.py
@@ -11,12 +11,16 @@ class CodeWriter:
         >>> writer = CodeWriter()
         >>> tests = yaml.load(open('test/CodeWriter.write.yaml').read())
         >>> for t in tests:
-        ...  writer.write(t['codedict'])
+        ...  writer.write(t['codedict'], depth=t['depth'])
         ...  print (t['codedict'])
         ...  print (t['expected'])
         ...  #assert t['codedict'] == t['expected']
         '''
-        pass
+        assert 'depth' in kwargs
+        indent = kwargs['depth'] * '    '
+        assert 'code' in codedict
+        assert 1 == len(codedict), str(codedict)
+        self.result += indent + codedict['code'].replace('\n', '\n' + indent)
 
 if __name__ == '__main__':
     import doctest
