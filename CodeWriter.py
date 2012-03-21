cb9789bfdf224034df6cbae6724e8d620846b5db
Taro WATASUE <wataro.mudamuda@gmail.com>
2012-03-22 06:39:18 +0900

    テストだけ書いた

--- CodeWriter.py
+++ CodeWriter.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-class CodeWriter: pass
+class CodeWriter:
+    def write(self, wagodict, **kwargs):
+        '''
+        >>> import yaml
+        >>> writer = CodeWriter()
+        >>> tests = yaml.load(open('test/CodeWriter.write.yaml').read())
+        >>> for t in tests:
+        ...  writer.write(t['wagodict'])
+        ...  print (t['wagodict'])
+        ...  print (t['expected'])
+        ...  #assert t['wagodict'] == t['expected']
+        '''
+        pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
